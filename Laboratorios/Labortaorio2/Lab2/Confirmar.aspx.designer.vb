6ef6ee67b8dc86c1f95a5efeeb249ff02afae34d
cristinamyr <cristinamayorotero@gmail.com>
2018-02-08 11:10:22 +0100

    Cambios en el labo

--- Laboratorios/Labortaorio2/Lab2/Confirmar.aspx.designer.vb
+++ Laboratorios/Labortaorio2/Lab2/Confirmar.aspx.designer.vb
@@ -30,4 +30,31 @@ Partial Public Class Confirmar
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control hl_inicio.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents hl_inicio As Global.System.Web.UI.WebControls.HyperLink
+    
+    '''<summary>
+    '''Control l_conf.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents l_conf As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control l_error.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents l_error As Global.System.Web.UI.WebControls.Label
 End Class
